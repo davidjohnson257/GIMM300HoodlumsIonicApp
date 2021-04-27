b13ac2c30cba7fb2014786d70e11d7c35071fa35
davidjohnson257 <78002129+davidjohnson257@users.noreply.github.com>
2021-04-26 22:09:08 -0600

    Added all of the calendar on Tab 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { fromEventPattern } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgCalendarModule} from 'ionic2-calendar';
+
+import { CalModalPageModule } from './pages/cal-modal/cal-modal.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  NgCalendarModule,
+CalModalPageModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
